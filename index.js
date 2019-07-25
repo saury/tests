3ed0456b296462abc87b05db642a15bffe22df8f
Jin Xu <jin.xu@ef.com>
2019-07-25 14:30:09 +0800

    feat: complete alert logic

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ class TemperatureConsole {
     this.freeze = data.freeze;
     this.boil = data.boil;
     this.fluctuation = data.fluctuation;
+    this.status = 'normal';
   }
 
   verifiedData(initData) {
@@ -21,6 +22,42 @@ class TemperatureConsole {
       }
     }
     return result;
+  }
+
+  alert(input) {
+    const inputs = input.split(' ');
+    return inputs
+      .reduce((acc, input) => {
+        acc.push(input);
+        // unboiling
+        if (
+          Number(input) < this.boil - this.fluctuation &&
+          this.status === 'boiling'
+        ) {
+          acc.push('unboiling');
+          this.status = 'normal';
+        }
+        // unfreezing
+        if (
+          Number(input) > this.freeze + this.fluctuation &&
+          this.status === 'freezing'
+        ) {
+          acc.push('unfreezing');
+          this.status = 'normal';
+        }
+        // boiling
+        if (Number(input) >= this.boil && this.status !== 'boiling') {
+          acc.push('boiling');
+          this.status = 'boiling';
+        }
+        // freezing
+        if (Number(input) <= this.freeze && this.status !== 'freezing') {
+          acc.push('freezing');
+          this.status = 'freezing';
+        }
+        return acc;
+      }, [])
+      .join(' ');
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -1,6 +1,12 @@
 const TemperatureConsole = require('./index');
 
 describe('Test For Temperature Console Programme', () => {
+  function alertResult(input) {
+    const tempConsole = new TemperatureConsole();
+    const result = tempConsole.alert(input);
+    return result;
+  }
+
   it('should have default startup val', () => {
     const tempConsole = new TemperatureConsole();
     expect(tempConsole.freeze).toBe(0);
@@ -32,5 +38,31 @@ describe('Test For Temperature Console Programme', () => {
     expect(tempConsole.freeze).toBe(0);
     expect(tempConsole.boil).toBe(100);
     expect(tempConsole.fluctuation).toBe(0.8);
+  });
+  it('should echo the within-range input value after calling alert fn', () => {
+    expect(alertResult('4.0')).toBe('4.0');
+  });
+  it('should echo the within-range input value repeatably by calling alert fn', () => {
+    expect(alertResult('4.0 3.0 1.0 99.0')).toBe('4.0 3.0 1.0 99.0');
+  });
+  it('should echo freezing alert if input val <= freeze threshold', () => {
+    expect(alertResult('-0.0')).toBe('-0.0 freezing');
+  });
+  it('should echo unfreezing alert if input val > freeze threshold + fluctuation val after triggering a freezing status', () => {
+    expect(
+      alertResult('4.0 1.0 0.5 0.0 -0.5 0.0 0.5 0.0 -2.0 0.0 0.5 0.6 2.0')
+    ).toBe(
+      '4.0 1.0 0.5 0.0 freezing -0.5 0.0 0.5 0.0 -2.0 0.0 0.5 0.6 unfreezing 2.0'
+    );
+  });
+  it('should echo boling alert if input val >= boiling threshold', () => {
+    expect(alertResult('5.0 -0.5 0.5 -0.2 100 101')).toBe(
+      '5.0 -0.5 freezing 0.5 -0.2 100 unfreezing boiling 101'
+    );
+  });
+  it('should echo unboiling alert if input val < boiling threshold - fluctuation val after triggering a boiling status', () => {
+    expect(alertResult('100 101 99.5 -1 1')).toBe(
+      '100 boiling 101 99.5 -1 unboiling freezing 1 unfreezing'
+    );
   });
 });
